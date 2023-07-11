c481cf0fce9897f768b198f1debeea60990ae837
Dario Piotrowicz <dario@cloudflare.com>
2023-07-11 19:18:52 +0100

    add access to cf platform example for Next.js

--- apps/next/app/api/hello/route.ts
+++ apps/next/app/api/hello/route.ts
@@ -6,5 +6,9 @@ import type { NextRequest } from 'next/server'
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
+  const myKv = process.env.MY_KV;
+  const allItems = await myKv.list();
+  console.log(allItems);
+  // NOTE: we cannot currently access ctx and cf
   return new Response(JSON.stringify({ name: 'John Doe' }))
 }
---
+++ apps/next/env.d.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    MY_KV: KVNamespace;
+  }
+}
